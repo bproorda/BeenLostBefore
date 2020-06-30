b14215d824dcf592e1eba6a5c24f9496afcd22e1
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-06-30 14:12:29 -0500

    got magnetometer working have trouble getting out the data

--- components/compass/index.js
+++ components/compass/index.js
@@ -1,35 +1,58 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
-import { Magnetometer } from 'expo-sensors';
+import { Magnetometer, Accelerometer, Gyroscope } from 'expo-sensors';
+import * as Permissions from 'expo-permissions';
+//import { magnetometer } from "react-native-sensors";
 
 
-export default function Compass(){
+export default function Compass() {
 
-const [magnetFound, setMagnetFound] = useState(false);
-const [data, setData] = React.useState({
-    x: 0,
-    y: 0,
-    z: 0,
-  });
+    const [magnetFound, setMagnetFound] = useState(false);
+    const [data, setData] = React.useState({
+        x: 0,
+        y: 0,
+        z: 0,
+    });
 
-
-useEffect(() =>{  
-async function initalSet(){
-   let result = await Magnetometer.isAvailableAsync();
-   console.log("The result is " + {result});
-   setMagnetFound(result);
-};
-}, []);
+  
 
 
+ // To run on initial render of page   
+    useEffect(() => {
+
+        alertIfRemoteNotificationsDisabledAsync()
+        initalSet();
+         Magnetometer.addListener(result => {
+             setData({ data: result });
+             //console.log(result);
+             console.log(data);
+           });
+        Magnetometer.setUpdateInterval(2000);
+
+    }, []);
+
+    async function initalSet() {
+        let result = await Magnetometer.isAvailableAsync();
+        console.log("The result is " + result);
+        setMagnetFound(result);
+    };
+
+    async function alertIfRemoteNotificationsDisabledAsync() {
+        const { status } = await Permissions.getAsync(Permissions.MOTION);
+        if (status !== 'granted') {
+            alert('Hey! You might want to enable notifications for my app, they are good.');
+        } else {
+            console.log("Permission Granted");
+        }
+    }
 
 
-
-return(
-    <>
-    <Text>underConstruction</Text>
-    <Text>{magnetFound ? "Compass Detected!" : "No Compass Detected!"}</Text>
-    </>
-)
+    return (
+        <>
+            <Text>underConstruction</Text>
+            <Text>{magnetFound ? "Compass Detected!" : "No Compass Detected!"}</Text>
+            <Text>X: {data.x} Y: {data.y} Z: {data.z}</Text>
+        </>
+    )
 
 }
