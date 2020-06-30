6d95d4c84927b36812c82f96d5a21196aba9e8b6
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-06-30 08:37:27 -0500

    did I miss a step?

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
+import compass from './images/compass.png';
 
 export default function App() {
+
+  const [value, onChangeText] = React.useState('Useless Placeholder');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>I've Been Lost Before and this is what it looks like</Text>
+      <Text>{value}</Text>
+      <Image source={compass} style={{width: 200, height: 200}}/>
+      <TextInput
+      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+      onChangeText={text => onChangeText(text)}
+      value={value}
+    />
       <StatusBar style="auto" />
     </View>
   );
